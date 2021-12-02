fb19f9c05716e45e9355c5950a0dbaa25f1b059c
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-12-02 11:17:23 -0500

    bug fix

--- src/views/RegistroPaciente.vue
+++ src/views/RegistroPaciente.vue
@@ -1,64 +1,63 @@
 <template>
-    <div class="container text-center">
-        <div class="col col-11 col-md-6">
-            <h2>Registrar Paciente</h2>
-            <form v-on:submit.prevent="processSignUp">
-                <div class="form-group text-left">
-                    <label for="">N�mero de Identificaci�n</label>
-                    <input type="number" v-model="patient.identification" />
-                </div>
-                <div class="form-group text-left">
-                    <label for="">Nombre Completo</label>
-                    <input type="text" v-model="patient.full_name" />
-                </div>
-                <div class="form-group text-left">
-                    <label for="">Fecha de Nacimiento</label>
-                    <input type="date" v-model="patient.date_of_birth" />
-                </div>
-                <div class="form-group text-left">
-                    <label for="">N�mero de Tel�fono</label>
-                    <input type="number" v-model="patient.phone" />
-                </div>
-                <div class="form-group text-left">
-                    <label for="">Estado Civil</label>
-                    <select v-model="patient.marital_status">
-                        <option disabled value="">Seleccione un Estado Civil</option>
-                        <option v-for="maritalStatus in maritalStatusArray" :key="maritalStatus">
-                            {{
-                                maritalStatus
-                            }}
-                        </option>
-                    </select>
-                </div>
-                <div class="form-group text-left">
-                    <label for="">Grupo Sangu�neo</label>
-                    <select v-model="patient.marital_status">
-                        <option disabled value="">Seleccione un Grupo Sangu�neo</option>
-                        <option v-for="bloodType in bloodTypesArray" :key="bloodType">
-                            {{
-                                bloodType
-                            }}
-                        </option>
-                    </select>
-                </div>
-                <div class="form-group text-left">
-                    <label for="">EPS</label>
-                    <select v-model="patient.eps">
-                        <option disabled value="">Seleccione una EPS</option>
-                        <option v-for="eps in EPSs"
-                                :key="eps"
-                                :value="eps.id">
-                            {{ eps.name }}
-                        </option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Registrar Paciente</button>
-                <button type="button" class="btn btn-primary" v-on:click="goBackHome">
-                    Volver
-                </button>
-            </form>
+  <div class="container text-center">
+    <div class="col col-11 col-md-6">
+      <h2>Registrar Paciente</h2>
+      <form v-on:submit.prevent="processSignUp">
+        <div class="form-group text-left">
+          <label for="">N�mero de Identificaci�n</label>
+          <input type="number" v-model="patient.identification" />
         </div>
+        <div class="form-group text-left">
+          <label for="">Nombre Completo</label>
+          <input type="text" v-model="patient.full_name" />
+        </div>
+        <div class="form-group text-left">
+          <label for="">Fecha de Nacimiento</label>
+          <input type="date" v-model="patient.date_of_birth" />
+        </div>
+        <div class="form-group text-left">
+          <label for="">N�mero de Tel�fono</label>
+          <input type="number" v-model="patient.phone" />
+        </div>
+        <div class="form-group text-left">
+          <label for="">Estado Civil</label>
+          <select v-model="patient.marital_status">
+            <option disabled value="">Seleccione un Estado Civil</option>
+            <option
+              v-for="maritalStatus in maritalStatusArray"
+              :key="maritalStatus"
+            >
+              {{ maritalStatus }}
+            </option>
+          </select>
+        </div>
+        <div class="form-group text-left">
+          <label for="">Grupo Sangu�neo</label>
+          <select v-model="patient.blood_type">
+            <option disabled value="">Seleccione un Grupo Sangu�neo</option>
+            <option v-for="bloodType in bloodTypesArray" :key="bloodType">
+              {{ bloodType }}
+            </option>
+          </select>
+        </div>
+        <div class="form-group text-left">
+          <label for="">EPS</label>
+          <select v-model="patient.idEps">
+            <option disabled value="">Seleccione una EPS</option>
+            <option v-for="eps in EPSs" :key="eps" :value="eps.id">
+              {{ eps.name }}
+            </option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Registrar Paciente
+        </button>
+        <button type="button" class="btn btn-primary" v-on:click="goBackHome">
+          Volver
+        </button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
@@ -68,36 +67,28 @@ export default {
   name: "RegistroPaciente",
   data: function() {
     return {
-        patient: {
-            identification: null,
-            full_name: "",
-            date_of_birth: null,
-            phone: null,
-            idHospital: null,
-        },
-        maritalStatusArray: [
-            "Soltero",
-            "Casado",
-            "Uni�n Libre",
-            "Viudo",
-            "Divorciado",
-        ],
-        bloodTypesArray: [
-            "A+",
-            "A-",
-            "AB+",
-            "AB-",
-            "B+",
-            "B-",
-            "O+",
-            "O-"
-        ],
-        EPSs: [],
+      patient: {
+        identification: null,
+        full_name: "",
+        date_of_birth: null,
+        phone: null,
+        blood_type: null,
+        marital_status: null,
+        idEps: null,
+      },
+      maritalStatusArray: [
+        "Soltero",
+        "Casado",
+        "Uni�n Libre",
+        "Viudo",
+        "Divorciado",
+      ],
+      bloodTypesArray: ["A+", "A-", "AB+", "AB-", "B+", "B-", "O+", "O-"],
+      EPSs: [],
     };
   },
   methods: {
     goBackHome: function() {
-      //TODO: cambiar cuando exista login
       this.$router.push({ name: "home" });
     },
     processSignUp: function() {
--- src/views/RegistroPacienteAntecedente.vue
+++ src/views/RegistroPacienteAntecedente.vue
@@ -1,32 +1,34 @@
 <template>
-    <div class="container text-center">
-        <div class="col col-11 col-md-6">
-            <h2>Registrar Antecedente</h2>
-            <form v-on:submit.prevent="processSignUp">
-                <div class="form-group text-left">
-                    <label for="">Numero de Documento del Paciente</label>
-                    <input type="number" v-model="paciente_antecedente.identification_patient" />
-                </div>
-
-                <div class="form-group text-left">
-                    <label for="">Antecedente</label>
-                    <select v-model="paciente_antecedente.idRecord">
-                        <option disabled value="">Seleccione un elemento</option>
-                        <option
-                        v-for="antecedente in antecedentes"
-                        :key="antecedente"
-                        :value="antecedente.id"
-                        >{{ antecedente.name }}</option
-                        >
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Vincular Antecedente con Paciente</button>
-                <button type="button" class="btn btn-primary" v-on:click="goBackHome">
-                    Volver
-                </button>
-            </form>
+  <div class="container text-center">
+    <div class="col col-11 col-md-6">
+      <h2>Registrar Antecedente</h2>
+      <form v-on:submit.prevent="processSignUp">
+        <div class="form-group text-left">
+          <label for="">Numero de Documento del Paciente</label>
+          <input type="number" v-model="paciente_antecedente.idPatient" />
         </div>
+
+        <div class="form-group text-left">
+          <label for="">Antecedente</label>
+          <select v-model="paciente_antecedente.idRecord">
+            <option disabled value="">Seleccione un elemento</option>
+            <option
+              v-for="antecedente in antecedentes"
+              :key="antecedente"
+              :value="antecedente.id"
+              >{{ antecedente.name }}</option
+            >
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Vincular Antecedente con Paciente
+        </button>
+        <button type="button" class="btn btn-primary" v-on:click="goBackHome">
+          Volver
+        </button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
@@ -36,21 +38,23 @@ export default {
   name: "VinculacionAntecedente",
   data: function() {
     return {
-        paciente_antecedente: {
-            identification_patient: null,
-            idRecord : null,
-        },
-        antecedentes : []
+      paciente_antecedente: {
+        idPatient: null,
+        idRecord: null,
+      },
+      antecedentes: [],
     };
   },
   methods: {
     goBackHome: function() {
-      //TODO: cambiar cuando exista login
       this.$router.push({ name: "home" });
     },
     processSignUp: function() {
       axios
-        .post(this.$store.state.backURL + "/vinculacion_antecedente/ingreso", this.patient_record)
+        .post(
+          this.$store.state.backURL + "/vinculacion_antecedente/ingreso",
+          this.paciente_antecedente
+        )
         .then((result) => {
           alert("Antecedente vinculado con �xito");
           this.goBackHome();
